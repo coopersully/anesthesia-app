f3f1d6437d6f6bc3b82cf32285023f9e3efe098d
Cooper Sullivan <thecoopersully@gmail.com>
2022-09-06 11:52:58 -0500

    Initial Commit

---
+++ Shared/AnesthesiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnesthesiaApp.swift
+//  Shared
+//
+//  Created by Cooper Sullivan on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct AnesthesiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
